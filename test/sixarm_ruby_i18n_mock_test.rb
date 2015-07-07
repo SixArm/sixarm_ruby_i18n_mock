0390d02a1baec7ec18e16610e92319c04c6a5f81
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:42 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_i18n_mock_test.rb
+++ test/sixarm_ruby_i18n_mock_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_i18n_mock'
+require "sixarm_ruby_i18n_mock"
 
 class Testing < Test::Unit::TestCase
 
